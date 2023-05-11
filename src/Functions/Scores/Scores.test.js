22733d9a8419a628d3c172f98cf14e832979643d
Richard Chimama <rich.chime@gmail.com>
2023-05-11 09:36:20 +0200

    fixed spelling error

--- src/Functions/Scores/Scores.test.js
+++ src/Functions/Scores/Scores.test.js
@@ -13,16 +13,19 @@ describe("Calculate the score",() => {
 
     
     test.each([
-        ["scissors","rock","fail"],
-        ["scissors","paper","win"],
-        ["paper","rock","win"],
-        ["paper","scissors","fail"],
-        ["rock","scissors","win"],
-        ["rock","paper","fail"],
-    ])("when user1=%s and user2=%s the result is %s",(user1, user2, result)=>{
-        const forUser1 = Scores(user1, user2) 
-        expect(forUser1).toBe(result)
-    })
+      ["scissors", "rock", "fail"],
+      ["scissors", "paper", "win"],
+      ["paper", "rock", "win"],
+      ["paper", "scissors", "fail"],
+      ["rock", "scissors", "win"],
+      ["rock", "paper", "fail"],
+    ])(
+      "when user1=%s and user2=%s the result is %s",
+      (user1, user2, result) => {
+        const forUser1 = Scores(user1, user2);
+        expect(forUser1).toBe(result);
+      }
+    );
     
     test("should determine the argument are declared in the function",()=>{
         const user1 = "scissors"
