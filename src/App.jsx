a5f4376d9b332c3a6b7291cd469d1a1da6e9fe3f
Richard Chimama <rich.chime@gmail.com>
2023-05-11 11:12:56 +0200

    cleaned the code and removed unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import PlayerSelect from './Components/PlayerSelect';
 import NameForm from './Components/NameForm';
 
 import Gameplay from './Gameplay';
-import localData from './LocalData';
 import './App.css'
 
 function App() {
-  const data = []//will hold an array of objects
   const [numPlayers, setNumPlayers] = useState(null);
   const [player1Name, setPlayer1Name] = useState('');
   const [player2Name, setPlayer2Name] = useState('');
   const [gameStarted, setGameStarted] = useState(false);
-  const [showplayerselect, setShowPlayerSelect] = useState('')
-  const[ showShowNameForm, setShowNameForm] = useState('')
+ 
 
   function handlePlayerSelect(num) {
     setNumPlayers(num);
-    setShowPlayerSelect(false);
-    setShowNameForm(true);
   }
 
   function handleNameSubmit(name1, name2) {
@@ -27,7 +22,6 @@ function App() {
     setGameStarted(true);
   }
   return (
-    <localData.Provider value={data} >
       <div id="container">
         {numPlayers === null && <PlayerSelect onSelect={handlePlayerSelect} />}
         {numPlayers !== null && player1Name === '' && player2Name === '' && (
@@ -40,7 +34,6 @@ function App() {
         {numPlayers === 1 && gameStarted && <Gameplay player1Name={player1Name} player2Name={"_computer_"}/>}
         {numPlayers === 2 && gameStarted && <Gameplay player1Name={player1Name} player2Name={player2Name}/>}
       </div>
-    </localData.Provider>
   );
 }
 
